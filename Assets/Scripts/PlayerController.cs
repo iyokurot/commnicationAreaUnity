c67e9dc46824160f6f6e0f2b42b0feb109efb409
iyokurot <01kayochin17neko@gmail.com>
2020-08-16 22:05:50 +0900

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    CharacterController characterController = default;
+    public float speed = 3.0f;
+    float moveX = 0f;
+    float moveZ = 0f;
+
+    void Update () {
+        moveX = Input.GetAxis ("Horizontal") * speed;
+        moveZ = Input.GetAxis ("Vertical") * speed;
+        Vector3 direction = new Vector3 (moveX, 0, moveZ);
+        characterController.SimpleMove (direction);
+        if (moveX != 0 || moveZ != 0) {
+            SocketClient.Instance.EmitMove (this.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/SocketClient.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using SocketIO;
+using UnityEngine;
+
+public class SocketClient : SingletonMonoBehaviourFast<SocketClient> {
+    SocketIOComponent socket;
+
+    public void Start () {
+        GameObject go = GameObject.Find ("SocketIO");
+        socket = go.GetComponent<SocketIOComponent> ();
+
+        socket.On ("online", OnOnline);
+        socket.On ("offline", OnOffline);
+
+        socket.On ("message", OnMessage);
+        socket.On ("move", OnMove);
+
+        //StartCoroutine ("BeepBoop");
+
+    }
+
+    private IEnumerator BeepBoop () {
+        // wait 1 seconds and continue
+        yield return new WaitForSeconds (1);
+
+        //socket.Emit("message");
+        EmitMessage ("text");
+
+        // wait 3 seconds and continue
+        yield return new WaitForSeconds (3);
+
+        //socket.Emit("message");
+        EmitMessage ("text");
+
+        // wait 2 seconds and continue
+        yield return new WaitForSeconds (2);
+
+        //socket.Emit("message");
+        EmitMessage ("text");
+
+        // wait ONE FRAME and continue
+        yield return null;
+
+        //socket.Emit("message");
+        //socket.Emit("message");
+        EmitMessage ("text");
+        EmitMessage ("text");
+    }
+
+    public void OnOnline (SocketIOEvent e) {
+        string id = e.data.GetField ("id").str;
+
+        Debug.Log ("online id: " + id);
+    }
+
+    public void OnOffline (SocketIOEvent e) {
+        string id = e.data.GetField ("id").str;
+
+        Debug.Log ("offline id: " + id);
+    }
+
+    public void OnMessage (SocketIOEvent e) {
+        JSONObject obj = e.data;
+
+        string id = obj.GetField ("id").str;
+        string message = obj.GetField ("message").str;
+
+        Debug.Log ("message id: " + id + " message: " + message);
+    }
+    public void OnMove (SocketIOEvent e) {
+        JSONObject obj = e.data;
+
+        string id = obj.GetField ("id").str;
+        float pos_x = obj.GetField ("x").n;
+        float pos_z = obj.GetField ("z").n;
+
+        Debug.Log ("message id: " + id + "pos_x " + pos_x + " pos_z " + pos_z);
+    }
+
+    public void EmitMessage (string message) {
+        JSONObject jsonObject = new JSONObject (JSONObject.Type.OBJECT);
+        jsonObject.AddField ("message", message);
+        jsonObject.AddField ("x", this.transform.position.x);
+        jsonObject.AddField ("y", this.transform.position.y);
+
+        socket.Emit ("message", jsonObject);
+    }
+
+    public void EmitMove (Vector3 pos) {
+        JSONObject jsonObject = new JSONObject (JSONObject.Type.OBJECT);
+        jsonObject.AddField ("x", pos.x);
+        jsonObject.AddField ("z", pos.z);
+
+        socket.Emit ("move", jsonObject);
+    }
+}
