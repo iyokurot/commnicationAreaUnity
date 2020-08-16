f8c871e9b457bf49c30b2d217301166961b3e520
iyokurot <01kayochin17neko@gmail.com>
2020-08-17 02:55:30 +0900

    リアルタイム位置同期の実装

--- Assets/Scripts/SocketClient.cs
+++ Assets/Scripts/SocketClient.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using SocketIO;
 using UnityEngine;
 
 public class SocketClient : SingletonMonoBehaviourFast<SocketClient> {
     SocketIOComponent socket;
+    [SerializeField]
+    GameObject playerObject = default;
+    private Dictionary<string, GameObject> playerTable = new Dictionary<string, GameObject> ();
 
     public void Start () {
         GameObject go = GameObject.Find ("SocketIO");
@@ -11,6 +15,7 @@ public class SocketClient : SingletonMonoBehaviourFast<SocketClient> {
 
         socket.On ("online", OnOnline);
         socket.On ("offline", OnOffline);
+        socket.On ("login_users", OnLoginUsers);
 
         socket.On ("message", OnMessage);
         socket.On ("move", OnMove);
@@ -49,14 +54,38 @@ public class SocketClient : SingletonMonoBehaviourFast<SocketClient> {
 
     public void OnOnline (SocketIOEvent e) {
         string id = e.data.GetField ("id").str;
+        // ユーザ生成
+        // 配列へ追加
+        GameObject obj = Instantiate (playerObject, new Vector3 (0, playerObject.transform.position.y, 0), playerObject.transform.rotation);
+        // Dictionaryに追加
+        playerTable.Add (id, obj);
 
         Debug.Log ("online id: " + id);
     }
 
     public void OnOffline (SocketIOEvent e) {
         string id = e.data.GetField ("id").str;
-
+        // 対象ユーザをDestroy
+        if (playerTable.ContainsKey (id)) {
+            Destroy (playerTable[id]);
+            playerTable.Remove (id);
+        }
         Debug.Log ("offline id: " + id);
+    }
+
+    public void OnLoginUsers (SocketIOEvent e) {
+        string id = e.data.GetField ("id").str;
+        string message = e.data.GetField ("message").str;
+        // ユーザを生成
+        message = message.Replace ("\\", string.Empty);
+        Users users = JsonUtility.FromJson<Users> (message);
+        foreach (var user in users.users) {
+            GameObject obj = Instantiate (playerObject, new Vector3 (user.pos_x, playerObject.transform.position.y, user.pos_z), playerObject.transform.rotation);
+            // Dictionaryに追加
+            playerTable.Add (user.id, obj);
+        }
+
+        Debug.Log ("message id: " + id + " message: " + message);
     }
 
     public void OnMessage (SocketIOEvent e) {
@@ -73,6 +102,17 @@ public class SocketClient : SingletonMonoBehaviourFast<SocketClient> {
         string id = obj.GetField ("id").str;
         float pos_x = obj.GetField ("x").n;
         float pos_z = obj.GetField ("z").n;
+
+        // 対象ユーザを探索し位置更新
+        foreach (KeyValuePair<string, GameObject> kvp in playerTable) {
+            if (kvp.Key == id) {
+                Vector3 pos = kvp.Value.transform.position;
+                pos.x = pos_x;
+                pos.z = pos_z;
+                kvp.Value.transform.position = pos;
+                break;
+            }
+        }
 
         Debug.Log ("message id: " + id + "pos_x " + pos_x + " pos_z " + pos_z);
     }
@@ -93,4 +133,15 @@ public class SocketClient : SingletonMonoBehaviourFast<SocketClient> {
 
         socket.Emit ("move", jsonObject);
     }
+}
+
+[System.Serializable]
+public class Users {
+    [System.Serializable]
+    public class Potisions {
+        public string id;
+        public int pos_x;
+        public int pos_z;
+    }
+    public Potisions[] users;
 }
